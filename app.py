6b6d1c9bc92ea1241017fafba4354ade3f14cdc8
Rick Lan <ricklan_blue@hotmail.com>
2022-03-18 23:32:00 -0400

    Edited app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    return render_template("index.html", firstname="Firstname", lastname="Lastname")
+    return "Hi World"
 
 
 if __name__ == "__main__":
